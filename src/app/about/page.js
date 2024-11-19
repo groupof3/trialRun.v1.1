ec3488dbc712a72fe1de5df526b2bc3d9a19fa45
Appiah Theophilus Boadi <boadiappiah112211@gmail.com>
2024-11-19 03:10:13 +0000

    check again

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        git add <p>
+         Our new page route.
+        </p>
+    </div>
+  )
+}
+
+export default page
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return <div>
     <details>
       <summary className={styles.emma}>See more</summary>
-      Give pull and push successful.
+      Git pull and push successful.
     </details>
     </div>;
 
