2570de85ef99fddb7fca3d2e9f3396e8dd8294d5
Adu emmanuel <isotopedegeneral@gmail.com>
2024-11-19 03:28:15 +0000

    emma

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,19 +1,21 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 const page = () => {
+  const [counter, setcounter] = useState(0);
+
+  const handdleAdd = () => {};
+  const handllSubtract = () => {};
+
   return (
     <div>
-        git add <p>
-         Our new page route.
-        </p>
-
-        <p>
-            <button>Increment +</button>
-            0
-            <button>Decrement -</button>
-        </p>
+      git add <p>Our new page route.</p>
+      <p>{0}</p>
+      <button onClick={handdleAdd}>Increment +</button>
+      <button onClick={handllSubtract}>Decrement -</button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
